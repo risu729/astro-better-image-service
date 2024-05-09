6efbad07707d581c24bae55e6402618a22a9f5d7
Risu <79110363+risu729@users.noreply.github.com>
2024-05-10 03:12:31 +0900

    fix(package.json): add sharp to dependencies (#81)
    * fix(package.json): add sharp to dependencies
    
    * chore(knip.config.ts): remove sharp from ignoreDependencies

--- knip.config.ts
+++ knip.config.ts
@@ -4,9 +4,6 @@ const config: KnipConfig = {
 	ignoreDependencies: [
 		// used in .husky/commit-msg, but knip doesn't work properly for bun run
 		"@commitlint/cli",
-		// sharp cannot be added to dependencies because of the Bun's bug
-		// ref: https://github.com/oven-sh/bun/issues/7729#issuecomment-2024889852
-		"sharp",
 	],
 	workspaces: {
 		".": {},
