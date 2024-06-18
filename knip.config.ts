b9cee9e8c1cac858840ae8d243535f4b2a0a1d1c
Risu <79110363+risu729@users.noreply.github.com>
2024-06-18 22:15:37 +0900

    chore(release.config.cjs): remove outdated workaround (#168)

--- knip.config.ts
+++ knip.config.ts
@@ -2,8 +2,9 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
 	ignoreDependencies: [
-		// used in .husky/commit-msg, but knip doesn't work properly for bun run
+		// bun run cannot be detected by Knip
 		"@commitlint/cli",
+		"semantic-release",
 	],
 	workspaces: {
 		".": {},
