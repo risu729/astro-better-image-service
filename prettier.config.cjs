958b12119a8bced1764f5e39cb7c68f2f405fb90
risu729 <git@risunosu.com>
2024-09-22 17:43:12 +0900

    chore(biome.jsonc): update config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,6 +3,8 @@
 // use `.cjs` and `module.exports` to avoid errors in VS Code
 // ref: https://github.com/prettier/prettier-vscode/issues/3066
 
+"use strict";
+
 /**
  * @type {import('prettier').Config}
  */
